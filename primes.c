20bdc958f6827f01d86a4de584ae76171326d44f
Michael <135757624+StudioGuma@users.noreply.github.com>
2023-12-16 11:58:05 -0500

    Update primes.c

--- primes.c
+++ primes.c
@@ -1,52 +1,52 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <inttypes.h>
 
-int main(void) {
-    unsigned long long n;
-    char c;
+int main(int argc, char** argv) {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: primes <n>");
+        return 1;
+    }
+    uint64_t n = atoi(argv[1]);
+    uint64_t init = n; // Store the input value, to know when to put multiplication signs
 
-    do {
-        printf("\nEnter a number: ");
-        scanf("%llu", &n);
-        unsigned long long init = n; // Store the input value, to know when to put multiplication signs
+    if (n < 2) {
+        fprintf(stderr, "Number must be at least 2");
+        return 1;
+    }
+    else {
+        printf("%" PRIu64 " = ", n);
 
-        if (n < 2) printf("Number must be at least 2");
-        else {
-            printf("%llu = ", n);
-
-            while (n % 2 == 0) {
-                if (n != init) printf(" * ");
-                printf("2");
-                n >>= 1; // n = n/2
-            }
-            while (n % 3 == 0) {
-                if (n != init) printf(" * ");
-                printf("3");
-                n /= 3;
-            }
-            while (n % 5 == 0) {
-                if (n != init) printf(" * ");
-                printf("5");
-                n /= 5;
-            }
-            unsigned long long val = 7;
-            while (val*val <= n) {
-                while (n % val == 0) {
-                    if (n != init) printf(" * ");
-                    printf("%llu", val);
-                    n /= val;
-                }
-                if (val % 10 == 3) val += 4;
-                else val += 2; // Skip multiples of 2 & 5
-            }
-            if (n != 1) {
-                if (n != init) printf(" * ");
-                printf("%llu", n);
-            }
+        while (n % 2 == 0) {
+            if (n != init) printf(" * ");
+            printf("2");
+            n >>= 1; // n = n/2
         }
-
-        printf("\nContinue? (y/n) ");
-        scanf("%s", &c);
-    } while ((c == 'y') || (c == 'Y'));
+        while (n % 3 == 0) {
+            if (n != init) printf(" * ");
+            printf("3");
+            n /= 3;
+        }
+        while (n % 5 == 0) {
+            if (n != init) printf(" * ");
+            printf("5");
+            n /= 5;
+        }
+        uint64_t val = 7;
+        while (val*val <= n) {
+            while (n % val == 0) {
+                if (n != init) printf(" * ");
+                printf("%" PRIu64, val);
+                n /= val;
+            }
+            if (val % 10 == 3) val += 4;
+            else val += 2; // Skip multiples of 2 & 5
+        }
+        if (n != 1) {
+            if (n != init) printf(" * ");
+            printf("%" PRIu64, n);
+        }
+    }
 
     return 0;
 }
