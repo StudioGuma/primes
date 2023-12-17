892aef502e76777361ed3fb51aad1b2b84a73505
Michael <135757624+StudioGuma@users.noreply.github.com>
2023-12-16 23:15:16 -0500

    Update primes.c

--- primes.c
+++ primes.c
@@ -17,7 +17,7 @@ int main(int argc, char** argv) {
         return 1;
     }
     uint64_t n = uatoll(argv[1]);
-    uint64_t init = n; // Store the input value, to know when to put multiplication signs
+    const uint64_t INIT = n; // Store the input value, to know when to put multiplication signs
 
     if (n < 2) {
         fprintf(stderr, "Number must be at least 2\n");
@@ -27,24 +27,24 @@ int main(int argc, char** argv) {
         printf("%" PRIu64 " = ", n);
 
         while (!(n & 1)) { // While n is divisible by 2
-            if (n != init) printf(" * ");
+            if (n != INIT) printf(" * ");
             printf("2");
             n >>= 1; // n = n/2
         }
         while (n % 3 == 0) {
-            if (n != init) printf(" * ");
+            if (n != INIT) printf(" * ");
             printf("3");
             n /= 3;
         }
         while (n % 5 == 0) {
-            if (n != init) printf(" * ");
+            if (n != INIT) printf(" * ");
             printf("5");
             n /= 5;
         }
         uint64_t val = 7;
         while (val*val <= n) {
             while (n % val == 0) {
-                if (n != init) printf(" * ");
+                if (n != INIT) printf(" * ");
                 printf("%" PRIu64, val);
                 n /= val;
             }
@@ -52,7 +52,7 @@ int main(int argc, char** argv) {
             else val += 2; // Skip multiples of 2 & 5
         }
         if (n != 1) {
-            if (n != init) printf(" * ");
+            if (n != INIT) printf(" * ");
             printf("%" PRIu64, n);
         }
     }
